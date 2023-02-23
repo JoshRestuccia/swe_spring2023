b614aec4de267a89c99a2d24fba434f29d022864
Carl Theodate <carl.theodate@ufl.edu>
2023-02-22 22:13:19 -0500

    fiber api

---
+++ investments/stock.go
@@ -0,0 +1,25 @@
+package stock
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetStocks(c *fiber.Ctx) error {
+	return c.SendString("return all stocks")
+
+}
+
+func GetStock(c *fiber.Ctx) error {
+	return c.SendString("return a single stock")
+
+}
+
+func AddStock(c *fiber.Ctx) error {
+	return c.SendString("Adds a new stock")
+
+}
+
+func DeleteStock(c *fiber.Ctx) error {
+	return c.SendString("Deletes a single stock")
+
+}
--- main.go
+++ main.go
@@ -1,39 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"github.com/gofiber/fiber/v2"
 )
 
+func helloWorld(c *fiber.Ctx) error {
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
-}
+	return c.SendString("Hello, World!")
 
-func handleRequests() {
-
-	myRouter := mux.NewRouter().StrictSlash(true)
-
-	myRouter.HandleFunc("/", homePage).Methods("GET")
-	myRouter.HandleFunc("/stocks", allStocks).Methods("GET")
-
-	myRouter.HandleFunc("/users", GetUsers).Methods("GET")
-	myRouter.HandleFunc("/users/{id}", GetUser).Methods("GET")
-	myRouter.HandleFunc("/users", CreateUser).Methods("POST")
-	myRouter.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
-	myRouter.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-
-	myRouter.HandleFunc("/stocks/{id}", AddStock).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
-	fmt.Println("Go ORM")
+	app := fiber.New()
 
-	InitialMigration()
-	handleRequests()
+	app.Get("/", helloWorld)
+
+	app.Listen(":3000")
 }
