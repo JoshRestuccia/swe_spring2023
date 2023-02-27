16b77fdbeeefaa4f1be586272cb05f5f4d2bbc2d
Oliver Peralta <olivercp2003@gmail.com>
2023-02-27 17:40:58 -0500

    Navigation between login and home pages

--- Portfullio/src/app/app-routing.module.ts
+++ Portfullio/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+// This .ts file helps us navigate between different pages
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/mainpage', pathMatch: 'full' }, // Here, the default route is set to the mainpage
+  { path: 'mainpage', component: MainpageComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Portfullio/src/app/app.module.ts
+++ Portfullio/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './login/login.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, MainpageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
