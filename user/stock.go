603f7d2b686f10c74db0e09fa162e2a0c60a14e1
Carl Theodate <carl.theodate@ufl.edu>
2023-02-27 17:26:00 -0500

    s

--- user/stock.go
+++
@@ -1,9 +0,0 @@
-package user
-
-type Stock struct {
-	Symbol    string  `json:"symbol"`
-	Name      string  `json:"name"`
-	Price     float64 `json:"price"`
-	UserRefer string  `json:"user_email"`
-	User      User    `gorm:"foreignKey:UserRefer"`
-}
