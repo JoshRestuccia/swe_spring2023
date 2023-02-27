668b874fe02501d8f1fd06bdd5d00ab2de430994
Carl Theodate <carl.theodate@ufl.edu>
2023-02-27 17:02:26 -0500

    fiber api

--- portfullio_backend/main.go
+++ portfullio_backend/main.go
@@ -1,38 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
+	"github.com/Carlfred3010/goApp/user"
+	"github.com/gofiber/fiber/v2"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Homepage Endpoint Hit")
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Hello, World!")
+
 }
 
-func handleRequests() {
+func Routers(app *fiber.App) {
+	app.Get("/users", user.GetUsers)
+	app.Get("/users/:id", user.GetUser)
+	app.Post("/user", user.SaveUser)
+	app.Delete("/user/:id", user.DeleteUser)
+	app.Put("/user/:id", user.UpdateUser)
 
-	myRouter := mux.NewRouter().StrictSlash(true)
-
-	myRouter.HandleFunc("/", homePage).Methods("GET")
-	myRouter.HandleFunc("/stocks", allStocks).Methods("GET")
-
-	myRouter.HandleFunc("/users", GetUsers).Methods("GET")
-	myRouter.HandleFunc("/users/{id}", GetUser).Methods("GET")
-	myRouter.HandleFunc("/users", CreateUser).Methods("POST")
-	myRouter.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
-	myRouter.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-
-	myRouter.HandleFunc("/stocks/{id}", AddStock).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
 func main() {
-	fmt.Println("Go ORM")
 
-	InitialMigration()
-	handleRequests()
+	user.InitialMigration()
+	app := fiber.New()
+
+	app.Get("/", hello)
+	Routers(app)
+
+	app.Listen(":3000")
+
 }
