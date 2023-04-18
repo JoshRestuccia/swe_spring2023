ad399dde600ce2d6309ad1891f39b90fc1e55604
Joshua Restuccia <joshua.restuccia@gmail.comm>
2023-04-17 23:58:01 -0400

    Added comments

--- Portfullio/src/app/login/login.component.ts
+++ Portfullio/src/app/login/login.component.ts
@@ -15,24 +15,30 @@ interface IUserItem {
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  //create variable to temporarily hold values for new objects
   public username = '';
   public password = '';
   public email = '';
+  //create an array to hold users to display on the page for debug purposes
   public userItems: IUserItem[] = [];
 
   constructor(private httpClient: HttpClient) {}
 
   async ngOnInit() {
+    //runs loadUsers on initialization of the component
     await this.loadUsers();
   }
 
   async loadUsers() {
+    //sends a get request that returns an array of IuserItem. the userItems array is then
+    //set to the returned value
     this.userItems = await firstValueFrom(
       this.httpClient.get<IUserItem[]>('/api/users')
     );
   }
 
   async addUser() {
+    //sends post request with the body of user and it's attributes
     this.httpClient
       .post<IUserItem>('/api/users', {
         username: this.username,
@@ -43,6 +49,7 @@ export class LoginComponent implements OnInit {
         console.log(response);
         this.loadUsers();
       });
+    //clears temp variables since they are connected to the html inputs
     this.username = '';
     this.password = '';
     this.email = '';
