42cf85ec30c28429f5a8ff0869576f5204b9c5ee
Oliver Peralta <olivercp2003@gmail.com>
2023-04-19 02:51:46 -0400

    Two Chart Examples for User's Portfolio

--- Portfullio/src/app/stocks/stocks.component.ts
+++ Portfullio/src/app/stocks/stocks.component.ts
@@ -12,16 +12,63 @@ export class StocksComponent {
       type: 'line',
     },
     title: {
-      text: 'Sample Stock Chart',
+      text: 'Your Portfolio the Past Months',
+    },
+    yAxis: {
+      title: {
+        text: 'Value in USD',
+      },
     },
     credits: {
       enabled: false,
     },
     series: [
       {
-        name: 'Price',
-        data: [140, 120, 180, 190, 185, 200, 230, 190, 240, 235, 255],
+        name: 'USD',
+        data: [
+          14000, 12000, 18000, 19000, 18500, 20000, 23000, 19000, 20040, 23005,
+          25500,
+        ],
       } as any,
+    ],
+  });
+  pieChart = new Chart({
+    chart: {
+      type: 'pie',
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
+    },
+    credits: {
+      enabled: false,
+    },
+    accessibility: {
+      point: {
+        valueSuffix: '%',
+      },
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+        },
+      },
+    },
+    series: [
+      {
+        colorByPoint: true,
+        type: 'pie',
+        data: [
+          { name: 'Stocks', y: 40.32, color: 'blue' },
+          { name: 'Real Estate', y: 10.2, color: 'red' },
+          { name: 'Cryptocurrency', y: 14.2, color: 'orange' },
+          { name: 'Bonds', y: 5.8, color: 'green' },
+          { name: 'Retirement', y: 29.48, color: 'yellow' },
+        ],
+      },
     ],
   });
 }
